0c27bf16df71dbf3be0edb63aea4148d759e9a05
barroqt <barroqt@gmail.com>
2022-03-26 17:55:26 +0100

    add percentage reward

--- contracts/Investment.sol
+++ contracts/Investment.sol
@@ -12,16 +12,22 @@ contract Investment is Initializable {
     address public investor;
 
     // Funding round
-    uint256 public fundingGoal;
-    uint256 public startDate; 
+    uint256 public fundingGoal; // Total campaign target
+    uint256 public startDate;
     uint256 public endDate;
-    uint256 public totalAllocatedAmount = 0;
+    uint256 public userSpentAmount = 0; // final size
+    uint256 public allocatedMaxAmount; // initial size
+    // uint256 public percentageDistributed;
+    // no use case yet, but would be useful to show the max potential token distributed if investors fulfills the allocation
+    uint256 public maxPercentageDistributed = (allocatedMaxAmount / fundingGoal) * 100;
+    uint256 public daoTokenAllocation = 1000000000000;
 
     // User balances
     mapping(address => uint) balances;
 
     // An Allocation has an Investor 
-    mapping(address => uint256) public investedAmount;
+    mapping(address => uint256) public investedAmount; // amount to be replaced with allocation struct
+    mapping(address => uint256) public percentageDistributed; // amount to be replaced with allocation struct
     // who already invested to avoid investing twice
     mapping(address => bool) public hasInvested;
 
@@ -29,10 +35,11 @@ contract Investment is Initializable {
 
     // TODO: allocation percentage: mapping(address => uint)
    
-    function init(uint256 _fundingGoal, uint256 _startDate, uint256 _endDate, address _token) external initializer {
+    function init(uint256 _fundingGoal, uint256 _allocatedMaxAmount, uint256 _startDate, uint256 _endDate, address _token) external initializer {
         investor = msg.sender;
         balances[investor] = investor.balance;
         fundingGoal = _fundingGoal;
+        allocatedMaxAmount = _allocatedMaxAmount;
         startDate = _startDate;
         endDate = _endDate;
         token = IERC20(_token);
@@ -46,21 +53,29 @@ contract Investment is Initializable {
     function depositAllocation(uint _amount, address _to) external payable {
         require(balances[msg.sender] >= _amount, "Not enough tokens");
         require(!hasInvested[msg.sender], "This user already invested");
-        require(totalAllocatedAmount + _amount <= fundingGoal, "Can't invest more than the targeted amount");
+        require(userSpentAmount + _amount <= fundingGoal, "Can't invest more than the campaign goal");
+        require(userSpentAmount + _amount <= allocatedMaxAmount, "Can't invest more than the allocated amount");
         require(block.timestamp >= startDate, "The campaign has not started yet");
         require(block.timestamp < endDate, "The campaign is over");
 
+        percentageDistributed[msg.sender] = (userSpentAmount / fundingGoal) * 100;
         hasInvested[msg.sender] = true; // We know this address is eligible for rewards
         investedAmount[msg.sender] = _amount; // We know the amount invested by this address
-        totalAllocatedAmount += _amount; // The amount of token invested for this campaign
+        userSpentAmount += _amount; // The amount of token invested for this campaign
         token.transferFrom(msg.sender, _to, _amount);
     }
-    
-    function sendRewards(uint _amount, address _to) external onlyAdmin() {
+
+    // todo : make sure dao has transfered enough tokens
+    function distributeToken(address[] memory _investors) external onlyAdmin() {
         require(block.timestamp > endDate, "The campaign is not over");
-        require(hasInvested[_to], "This user did not invest");
-        hasInvested[msg.sender] = false;
-        // TODO add percentage reward
-        token.transferFrom(msg.sender, _to, _amount);
+        for (uint i = 0; i < _investors.length; i++) {
+            require(hasInvested[_investors[i]], "This user did not invest");
+            hasInvested[_investors[i]] = false;
+            uint amountToSend = (percentageDistributed[_investors[i]] / 100) * daoTokenAllocation;
+            token.transferFrom(msg.sender, _investors[i], amountToSend);
+        }
     }
+
+    // TODO: if finalSize < initialSize : start another round
+
 }
