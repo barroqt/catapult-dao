d585b0c0e8d339b9c3c61df067b3a7df418f7034
King Diamond Ape <star.ascendin@gmail.com>
2022-03-26 16:50:45 +0100

    WIP -- testing createAlloc

--- contracts/Investment.sol
+++ contracts/Investment.sol
@@ -6,6 +6,9 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 // TODO(system): implement logic around DAO (deposit, withdraw, distribute)
 contract Investment is Initializable, AccessControl {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant INVESTOR_ROLE = keccak256("INVESTOR_ROLE");
+
 
     address public admin;
     uint256 public fundingGoal;
@@ -14,6 +17,8 @@ contract Investment is Initializable, AccessControl {
     uint256 public endDate;
 
     Allocation[] public allocations;
+    // investor key => allocations index
+    mapping(address => uint) public allocationsMap;
 
     function init(
         address _admin,
@@ -22,11 +27,13 @@ contract Investment is Initializable, AccessControl {
         uint256 _startDate,
         uint256 _endDate
         ) external initializer {
-        admin = _admin;
-        fundingGoal = _fundingGoal;
-        fundingToken = _fundingToken;
-        startDate = _startDate;
-        endDate = _endDate;
+            admin = _admin;
+            fundingGoal = _fundingGoal;
+            fundingToken = _fundingToken;
+            startDate = _startDate;
+            endDate = _endDate;
+
+            _setupRole(ADMIN_ROLE, admin);
     }
 
     struct Allocation {
@@ -35,7 +42,25 @@ contract Investment is Initializable, AccessControl {
         uint256 fundedSize;
     }
 
-    function createAllocations(address investor, uint256 allocationSize ) private {
+    // caller can be anyone
+    function createAllocation(
+            address _investor, 
+            uint256 _allocationSize
+        ) public returns (Allocation memory) {
+        // Check Role
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an Admin");
 
+        Allocation memory alloc = Allocation({
+            investor: _investor,
+            allocationSize: _allocationSize,
+            fundedSize: 0
+        });
+        uint idxLength = allocations.length;
+        allocations.push(alloc);
+        allocationsMap[_investor] = idxLength;
+        return alloc;
     }
+
+    
+
 }
--- test/DAOFundingFactory.ts
+++ test/DAOFundingFactory.ts
@@ -3,8 +3,11 @@ const { BigNumber, utils } = require('ethers');
 const { ethers } = require('hardhat');
 
 describe('DAOFundingFactory', function () {
+    let user01
+    let owner
     before(async function () {
-        const [owner, admin] = await ethers.getSigners();
+        const [owner, signer] = await ethers.getSigners();
+        user01 = signer
         this.Investment = await ethers.getContractFactory("Investment", owner);
         this.DAOFundingFactory = await ethers.getContractFactory("DAOFundingFactory", owner);
     });
@@ -21,9 +24,9 @@ describe('DAOFundingFactory', function () {
     //  DAOFundingFactory
     //////////////////////////////
     describe("DAOFundingFactory", function () {
-        const admin = '0xdff7b4C69571eCfc041b9434Bb600639Cea175Cc'
+        const adminAddress = '0xdff7b4C69571eCfc041b9434Bb600639Cea175Cc'
         const usdc = '0x45ea5d57BA80B5e3b0Ed502e9a08d568c96278F9'
-        const args = [admin,
+        const args = [adminAddress,
             1000,
             usdc,
             100,
@@ -34,20 +37,59 @@ describe('DAOFundingFactory', function () {
         });
 
         it('can create Investment clones', async function () {
-
             for (let i = 0; i < 10; i++) {
                 await this.daoFundingFactory.createDAOFunding(...args);
                 const investmentChildAddress = this.daoFundingFactory.investments(i);
                 const investmentChildContract = this.investment.attach(investmentChildAddress);
-                expect(await investmentChildContract.admin()).to.eq(admin);
+                expect(await investmentChildContract.admin()).to.eq(adminAddress);
             }
         });
 
         it('emits the InvestmentCreated event', async function () {
-            const adminAddress = '0xdff7b4C69571eCfc041b9434Bb600639Cea175Cc'
             await expect(this.daoFundingFactory.createDAOFunding(...args)).to
                 .emit(this.daoFundingFactory, 'InvestmentCreated')
                 .withArgs(await this.daoFundingFactory.investments(0));
+        })
+
+        // TEST ADMIN RBAC
+        describe("#createAllocation", function () {
+            it('throw error if called by user without ADMIN role ', async function () {
+                await this.daoFundingFactory.createDAOFunding(...args);
+                const investmentChildAddress = this.daoFundingFactory.investments(0);
+                const investmentChildContract = this.investment.attach(investmentChildAddress);
+                const investorAddr = '0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199'
+                await expect(investmentChildContract.connect(user01).createAllocation(
+                    investorAddr,
+                    1000
+                )).to.be.revertedWith('Caller is not an Admin')
+            })
+
+            it('should pass  if called by user with ADMIN role ', async function () {
+                const adminArgs = [...args]
+                adminArgs[0] = user01.getAddress()
+                await this.daoFundingFactory.createDAOFunding(...adminArgs);
+                const investmentChildAddress = this.daoFundingFactory.investments(0);
+                const investmentChildContract = this.investment.attach(investmentChildAddress);
+                // const investorAddr = '0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199'
+                // TODO FIX this test
+                const investorAddrs = ['0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199', '0xdd2fd4581271e230360230f9337d5c0430bf44c0']
+                for (let i = 0; i < investorAddrs.length; i++) {
+                    const investorAddr = investorAddrs[i]
+                    expect(await investmentChildContract.connect(user01).createAllocation(
+                        investorAddr,
+                        1000
+                    )).to.have.property('hash')
+                    const expectedAlloc = (await investmentChildContract.connect(user01).allocations(i))
+                    expect(expectedAlloc.length).to.eq(3)
+                    expect(expectedAlloc.allocationSize).to.eq(1000)
+                    expect(expectedAlloc.investor.toLowerCase()).to.eq(investorAddr.toLowerCase())
+                    expect(expectedAlloc.fundedSize).to.eq(0)
+    
+                    const investorIdx = (await investmentChildContract.connect(user01).allocationsMap(investorAddr))
+                    expect(investorIdx).to.eq(i)
+
+                }
+            })
         })
     });
 
